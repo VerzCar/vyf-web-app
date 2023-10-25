8a44553037edb345b202357d1b5b04137ebaecbd
verzcar <carlo.verzeri@vecomentman.de>
2023-10-25 11:28:43 +0200

    append auth interceptor header

--- src/app/core/auth/auth-interceptor.interceptor.ts
+++ src/app/core/auth/auth-interceptor.interceptor.ts
@@ -1,27 +1,19 @@
-import {inject, Injectable} from '@angular/core';
-import {
-  HttpRequest,
-  HttpHandler,
-  HttpEvent,
-  HttpInterceptor
-} from '@angular/common/http';
-import {Observable, switchMap} from 'rxjs';
-import {AwsCognitoService} from '../services/aws-cognito.service';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable, switchMap } from 'rxjs';
+import { AwsCognitoService } from '../services/aws-cognito.service';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  private readonly awsCognitoService = inject(AwsCognitoService);
+    private readonly awsCognitoService = inject(AwsCognitoService);
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-	return this.awsCognitoService.getCurrentSession().pipe(
-	  switchMap(session => {
-		request = request.clone({
-		  setHeaders: {
-			'Accept': 'application/json',
-			'Authorization': `Bearer ${session.getAccessToken().getJwtToken()}`,
-		  },
-		});
-		return next.handle(request);
-	  }));
-  }
+    public intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+        return this.awsCognitoService.getCurrentSession().pipe(
+            switchMap(session => {
+                const newRequest = request.clone({
+                    headers: request.headers.append('Authorization', `Bearer ${session.getAccessToken().getJwtToken()}`)
+                });
+                return next.handle(newRequest);
+            }));
+    }
 }
