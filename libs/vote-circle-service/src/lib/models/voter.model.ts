6617c019854cb06c95f1e723cc3f6f7f4574bea2
verzcar <carlo.verzeri@vecomentman.de>
2023-10-27 21:23:38 +0200

    added new voter fields

--- libs/vote-circle-service/src/lib/models/voter.model.ts
+++ libs/vote-circle-service/src/lib/models/voter.model.ts
@@ -1,9 +1,11 @@
 import { Commitment } from './commitment.model';
 
 export interface Voter {
-  id: number;
-  voter: string;
-  commitment: Commitment;
-  createdAt: Date;
-  updatedAt: Date;
+    id: number;
+    voter: string;
+    commitment: Commitment;
+    votedFor: string | null;
+    votedFrom: string | null;
+    createdAt: Date;
+    updatedAt: Date;
 }
