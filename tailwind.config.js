6344ab03828cdd24f8c49cb2a20021611c44a01c
Verzeri, C., ext., SNL IT P&P, BN, external <carlo.verzeri@deutschepost.de>
2023-11-28 16:27:08 +0100

    upgraded to v17

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,6 @@ const { join } = require('path');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: 'jit',
-  darkMode: false,
   content: [
     join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
     ...createGlobPatternsForDependencies(__dirname),
