89174cbbf71353313cb13042b8fee5e9c343c2cb
verzcar <carlo.verzeri@vecomentman.de>
2024-01-06 13:12:35 +0100

    updated circle update model

--- libs/vote-circle-service/src/lib/models/circle-update-request.model.ts
+++ libs/vote-circle-service/src/lib/models/circle-update-request.model.ts
@@ -6,7 +6,6 @@ export interface CircleUpdateRequest {
     readonly voters?: VoterRequest[];
     readonly description?: string;
     readonly imageSrc?: string;
-    readonly private?: boolean;
     readonly delete?: boolean;
     readonly validUntil?: Date;
 }
