553f7cc408ac688cf976f02f7a659682f11d81b2
verzcar <carlo.verzeri@vecomentman.de>
2024-03-21 08:23:21 +0100

    changed to new user pool

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -4,14 +4,16 @@
 const awsmobile = {
     "aws_project_region": "eu-central-1",
     "aws_cognito_region": "eu-central-1",
-    "aws_user_pools_id": "eu-central-1_pX1mKgETU",
-    "aws_user_pools_web_client_id": "73ot9gku0nb41smmecdm7jbn73",
+    "aws_user_pools_id": "eu-central-1_3bcD2yICY",
+    "aws_user_pools_web_client_id": "7g74hkmbniv48k5f1j159a1duu",
     "oauth": {
-        "domain": "vyf-user-idp.auth.eu-central-1.amazoncognito.com"
+        "domain": "vyf-user.auth.eu-central-1.amazoncognito.com"
     },
     "aws_cognito_username_attributes": [],
     "aws_cognito_social_providers": [],
-    "aws_cognito_signup_attributes": [],
+    "aws_cognito_signup_attributes": [
+        "EMAIL"
+    ],
     "aws_cognito_mfa_configuration": "OPTIONAL",
     "aws_cognito_mfa_types": [
         "SMS",
